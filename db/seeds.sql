602ebddace38b752cd67344259a8798932b0ac90
Ryan Garza <ryangarza@live.com>
2020-10-26 21:11:14 -0500

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
